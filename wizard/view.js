ddc6cd5e23ad61de29e637532ad4f118fa1274da
TIVMOF <tomyivanov06@gmail.com>
2024-04-10 09:42:37 +0000

    Trying dialog window

--- wizard/view.js
+++ wizard/view.js
@@ -1,25 +1,13 @@
-exports.getAction = function () {
-    return {
-        id: 'wizard-helper',
-        label: 'Wizard',
-        perspective: 'purchaseinvoice',
-        view: "PurchaseInvoice",
-        type: "page",
-        link: '/services/web/codbex-invoices-ext/wizard/wizard.html',
+const viewData = {
+    id: 'wizard-helper',
+    label: 'Wizard',
+    perspective: 'purchaseinvoice',
+    view: "PurchaseInvoice",
+    link: '/services/web/codbex-invoices-ext/wizard/wizard.html'
+};
+
+if (typeof exports !== 'undefined') {
+    exports.getDialogWindow = function () {
+        return viewData;
     }
 }
-
-// const viewData = {
-//     id: 'wizard-helper',
-//     label: 'Wizard',
-//     perspective: 'purchaseinvoice',
-//     view: "PurchaseInvoice",
-//     type: "page",
-//     link: '/services/web/codbex-invoices-ext/wizard/wizard.html'
-// };
-
-// if (typeof exports !== 'undefined') {
-//     exports.getAction = function () {
-//         return viewData;
-//     }
-// }
